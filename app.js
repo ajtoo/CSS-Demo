eb9d0b2f9c6691452bcbd891e711513cdd469a80
ajtoo <ajtoo.dev@gmail.com>
2017-03-28 20:54:39 -0700

    smoothed using transition

--- app.js
+++ app.js
@@ -2,45 +2,51 @@ function autorun()
 {
   console.log("autorunning");
   let titleNav = document.querySelector(".title-nav")
-  fixNavOnScroll(titleNav, 75);
+  fixNavOnScroll(titleNav);
 }
 if (window.addEventListener) window.addEventListener("load", autorun, false);
 else if (window.attachEvent) window.attachEvent("onload", autorun);
 else window.onload = autorun;
 
-//TODO: debounce
+//TODO: debounce and determine scroll direction
 //takes height in vh (recommend +5vh from height of item)
-function fixNavOnScroll(nav, height) {
+function fixNavOnScroll(nav) {
+  var prevScrollPos = 0
+  let navTopFixed = document.querySelector(".nav-top-fixed");
+  let fixNavHeight = navTopFixed.offsetHeight;
   window.addEventListener("scroll", (e) => {
     let lastScrollPos = window.scrollY;
-    let swapWaypoint = getViewHeight() - 136;  //vh
-    let transitionOutWaypoint = getViewHeight() - 200;
-    let transitionInWaypoint = getViewHeight();
-    console.log(window.scrollY)
-    if(lastScrollPos > transitionOutWaypoint) {
-      (transitionOutFixedNavbar = () => {
-        let navTopFixed = document.querySelector(".nav-top-fixed");
+    let vh = getViewHeight();
+    let vhOne = vh * .01;
+    let swapWaypoint = vh - nav.offsetHeight;
+
+    let transitionHalfWaypoint = vh - (nav.offsetHeight + fixNavHeight + vhOne);
+    let transitionInWaypoint = transitionHalfWaypoint  + vhOne;
+
+    if(lastScrollPos > transitionHalfWaypoint && lastScrollPos < swapWaypoint && navTopFixed.style.height != 0 && navTopFixed.style.height != "5vh") {
+      console.log("first collapse")
         if(navTopFixed !== null) {
-          // lastScrollPos - swapWaypoint
-          navTopFixed.style.height = 68;
+          navTopFixed.style.height = "5vh";
         }
-      })();
-    } else if(lastScrollPos <= transitionInWaypoint) {
-      (transitionInFixedNavbar = () => {
-        let navTopFixed = document.querySelector(".nav-top-fixed");
+    } else if (lastScrollPos > swapWaypoint - fixNavHeight* 0.5) {
+      console.log("second collapse", lastScrollPos, swapWaypoint - fixNavHeight * .75);
+      navTopFixed.style.height = "0";
+    } else if(lastScrollPos < transitionInWaypoint && lastScrollPos < prevScrollPos) {
+      console.log("expand")
         if(navTopFixed !== null) {
           navTopFixed.style.height = "10vh";
         }
-      })();
     }
 
-    if(lastScrollPos > swapWaypoint && !nav.className.includes("nav-top-fixed")) {
+    if(lastScrollPos > swapWaypoint &&  !nav.className.includes("nav-top-fixed")) {
       nav.classList.add("nav-top-fixed");
       nav.classList.remove("nav-bottom");
     } else if(lastScrollPos <= swapWaypoint && nav.className.includes("nav-top-fixed")) {
       nav.classList.remove("nav-top-fixed");
     }
+    prevScrollPos = lastScrollPos;
   });
+
 }
 
 //only supports standards compliant browsers
